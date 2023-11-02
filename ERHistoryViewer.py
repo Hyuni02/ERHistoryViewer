776ce8a85f32cbd23aa58196a84409d7877c1625
ErrorCode02 <jackjsh@naver.com>
2023-11-03 00:04:51 +0900

    GetUserNum

--- ERHistoryViewer.py
+++ ERHistoryViewer.py
@@ -1,1 +1,13 @@
 import requests
+
+key = "AcUmvv9Rtp2aOoVKiDnqP4gdVzeqiTVYahP9Xi6U"
+
+baseURL="https://open-api.bser.io/"
+header = {"x-api-key":f"{key}"}
+
+# nickName으로 userNum 찾기
+def GetUserNum(nickName):
+    response_userNum = requests.get(f"{baseURL}v1/user/nickname?query={nickName}", headers=header)
+    print("userNum :", response_userNum.json()["user"]["userNum"])
+    return response_userNum.json()["user"]["userNum"]
+

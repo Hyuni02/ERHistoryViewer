ad369e7dee330838b9833dfc08fcb1f5fefcbd1e
ErrorCode02 <jackjsh@naver.com>
2023-11-20 23:04:53 +0900

    tkinter를 쓰기위한 수많은 노력들

--- PageViwer.py
+++ PageViwer.py
@@ -1,24 +1,36 @@
 import tkinter as tk
+import page_main
 
 window = tk.Tk()
+
+# 화면 : 메인
+def Show_Main():
+    # UI 생성
+    lab_logo = tk.Label(window, text="[LOGO]")
+    etr_userName = tk.Entry(window)
+    lab_reuslt = tk.Label(window, text="")
+    btn_search = tk.Button(window, text='search', command=lambda: SearchUser(etr_userName.get()))
+
+    # UI 배치
+    lab_logo.pack()
+    etr_userName.pack()
+    lab_reuslt.pack()
+    btn_search.pack()
+
+def SearchUser(userName):
+    print(userName)
+
+
 
 # 윈도우 제목 지정
 window.title('ERHistoryViwer')
 # 윈도우 아이콘 지정
-# window.iconphoto(False, tk.PhotoImage(file="icons/???.png"))
+# window.iconphoto(False, tk.PhotoImage(file="icons/winicon.png"))
 # 윈도우 크기 지정
-window.geometry("600x600")
-window.resizable(False,True)
+window.geometry("600x600+50+50")
+window.resizable(False, True)
 
-# 버튼 추가
-style1 = {'bg':'green', 'fg':'black'}
-style2 = {'bg':'black', 'fg':'white'}
-btn_TL = tk.Button(window, text="btn_TL", **style1)
-btn_TR = tk.Button(window, text="btn_TR", **style2)
+Show_Main()
 
-# UI 배치
-btn_TL.pack(side="top")
-btn_TR.pack(side="right", anchor="nw")
 # 윈도우 시작
 window.mainloop()
-
---
+++ page_main.py
@@ -0,0 +1,40 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("Frame_Change")
+window.geometry("600x600+200+200")
+
+frame1 = tkinter.Frame(window)
+frame2 = tkinter.Frame(window)
+frame3 = tkinter.Frame(window)
+
+frame1.grid(row=0, column=0, sticky="nsew")
+frame2.grid(row=0, column=0, sticky="nsew")
+frame3.grid(row=0, column=0, sticky="nsew")
+
+def openFrame(frame):
+    frame.tkraise()
+
+
+btnToFrame1 = tkinter.Button(frame3,
+                             text="Change To Frame1",
+                             padx=10,
+                             pady=10,
+                             command=lambda: [openFrame(frame1)])
+btnToFrame2 = tkinter.Button(frame1,
+                             text="Change To Frame2",
+                             padx=10,
+                             pady=10,
+                             command=lambda: [openFrame(frame2)])
+btnToFrame3 = tkinter.Button(frame2,
+                             text="Change To Frame3",
+                             padx=10,
+                             pady=10,
+                             command=lambda: [openFrame(frame3)])
+
+btnToFrame1.pack()
+btnToFrame2.pack()
+btnToFrame3.pack()
+
+openFrame(frame1)  # 기본메인화면
+window.mainloop()
